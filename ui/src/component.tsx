41334a3bf2a51360f947f3af18e6735d13569335
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-04-08 14:37:07 -0400

    nit changes

--- ui/src/component.tsx
+++ ui/src/component.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { AppBar, Paper, Typography, colors } from "@mui/material";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import { color } from "@mui/system";
-import { green } from "@mui/material/colors";
+import { AppBar, Paper, Typography } from "@mui/material";
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
 
 export default class Component extends React.Component<any, any> {
   constructor(props: any) {
@@ -24,48 +26,50 @@ export default class Component extends React.Component<any, any> {
         }}
       >
         <Paper elevation={3} style={{ height: "100%", width: "100%" }}>
-          <div id="title" style={{ height: "30%", width: "100%" }}>
-            <AppBar position="static" style={{backgroundColor: "#8bc34a"}}>
+          <div id="title" style={{ width: "100%" }}>
+            <AppBar position="static" style={{ backgroundColor: "#8bc34a" }}>
               <Typography variant="h2" noWrap>
                 Greenify Me!!
               </Typography>
             </AppBar>
           </div>
-          <div id="body" style={{ height: "70%", width: "100%" }}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-              }}
-            >
-              <Typography variant="h3" style={{ width: "fit-content" }}>
-                {"Your current score : " + this.state.currentScore}
-              </Typography>
+          <div id="body" style={{ marginTop: "5px", width: "100%" }}>
+            <AppBar position="static" color="transparent">
               <div
                 style={{
-                  width: "50px",
-                  height: "50px",
-                  marginLeft: "10px",
-                  marginRight: "10px",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
                 }}
               >
-                <CircularProgressbar
-                  value={parseInt(this.state.currentScore)}
-                  maxValue={100}
-                  styles={buildStyles({
-                    rotation: 0.25,
-                    strokeLinecap: "butt",
-                    textSize: "16px",
-                    pathTransitionDuration: 0.5,
-                    pathColor: "#8BC34A",
-                    textColor: "#f88",
-                    trailColor: "#FFCCBC",
-                    backgroundColor: "#3e98c7",
-                  })}
-                />
+                <Typography variant="h3" style={{ width: "fit-content" }}>
+                  {"Your current score : " + this.state.currentScore + "%"}
+                </Typography>
+                <div
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    marginLeft: "10px",
+                    marginRight: "10px",
+                  }}
+                >
+                  <CircularProgressbarWithChildren
+                    value={parseInt(this.state.currentScore)}
+                    maxValue={100}
+                    styles={buildStyles({
+                      rotation: 0.25,
+                      strokeLinecap: "butt",
+                      pathTransitionDuration: 0.5,
+                      pathColor: "#8BC34A",
+                      textColor: "#f88",
+                      trailColor: "#FFCCBC",
+                      backgroundColor: "#3e98c7",
+                    })}
+                  >
+                  </CircularProgressbarWithChildren>
+                </div>
               </div>
-            </div>
+            </AppBar>
           </div>
         </Paper>
       </div>
