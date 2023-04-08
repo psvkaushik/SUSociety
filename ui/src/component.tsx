45d27037c8e2eaa7430249aa3bffabd9c7c0e709
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-04-08 17:58:50 -0400

    fix leaderboard table

--- ui/src/component.tsx
+++ ui/src/component.tsx
@@ -190,7 +190,7 @@ export default class Component extends React.Component<any, any> {
     return (
       <div>
         <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <Table aria-label="simple table">
             <TableHead>
               <TableRow>
                 <TableCell>Name</TableCell>
